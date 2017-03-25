d11d3303135ff2bc2590734b972ae37d5bdc5908
egg82 <phantom_zero@ymail.com>
2017-03-25 01:41:41 -0600

    Added CommandEvent

---
+++ src/ninja/egg82/events/CommandEvent.java
@@ -0,0 +1,17 @@
+package ninja.egg82.events;
+
+public class CommandEvent {
+	//vars
+	public static final String COMPLETE = "complete";
+	public static final String ERROR = "error";
+	
+	//constructor
+	public CommandEvent() {
+		
+	}
+	
+	//public
+	
+	//private
+	
+}
